9baf4d79bd515ceced3574e1ce96cfc3761b15b1
youssief101 <30203051305119@sci.asu.edu.eg>
2023-08-14 18:50:46 +0300

    adding mental-math-training-program

--- addition-mental-math-training.cpp
+++ addition-mental-math-training.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 #include <random>
+#include <cmath>
 using namespace std;
 
 void create(int dn, int qn) {
+  int minNumber = pow(10, dn - 1);
+  int maxNumber = pow(10, dn) - 1;
   // create a random number generator engine and seed it
   random_device rd;
   mt19937 gen(rd());
 
   // define the distribution range for two-digit numbers
-  uniform_int_distribution<int> dis(10, 99);
+  uniform_int_distribution<int> dis(minNumber, maxNumber);
   
   for (int i = 0; i < qn; i++) {
     // create qn of dn
     int num1 = dis(gen);
     int num2 = dis(gen);
-    cout << num1 + num2 << " = " << num1 + num2 << endl;
+    cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
   }
 }
 
@@ -29,7 +32,12 @@ int main() {
   cout << "Enter the number of exercises: ";
   cin >> qNum;
 
-  // create qNum of digNum questions:
-  create(digNum, qNum);
+  if (digNum <= 0 || digNum > 9) {
+    cout << "invalid number of digits." << endl;
+    return -1;
+  } else {
+    // create qNum of digNum questions:
+    create(digNum, qNum);
+  }
   return 0;
 }

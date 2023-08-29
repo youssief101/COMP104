23d4685130a7592a69f1beabc372c8ee21c74416
youssief101 <30203051305119@sci.asu.edu.eg>
2023-08-29 21:52:40 +0300

    update

--- fiveInOne.cpp
+++ fiveInOne.cpp
@@ -2,117 +2,82 @@
 #include <cmath>
 using namespace std;
 
+// check prime numbers
 bool isPrime(int num) {
-  if (num <= 1)
-    return false;
-  for (int i = 2; i <= sqrt(num); i++)
-    if (num % i == 0)
+    if (num <= 1)
       return false;
+    if (num <= 3)
+      return true;
+    if (num % 2 == 0 || num % 3 == 0)
+      return false;
+    
+    for (int i = 5; i * i <= num; i += 6) {
+        if (num % i == 0 || num % (i + 2) == 0)
+            return false;
 
-  return true;
-}
-
-int reverse(int num) {
-  int reversed = 0;
-  while (num > 0) {
-    reversed = num % 10 + reversed * 10;
-    num /= 10;
-  }
-  
-  return reversed;
-}
-
-bool isPalindrome(int ordered, int reversed) {
-  if (ordered == reversed)
     return true;
-
-  return false;
 }
 
-int findMax(int arr[], const int size) {
-  int max = arr[0];
-  for (int i = 0; i < size; i++)
-    if (max < arr[i])
-      max = arr[i];
-  return max;
-}
-
-int findMin(int arr[], const int size) {
-  int min = arr[0];
-  for (int i = 0; i < size; i++)
-    if (min > arr[i])
-      min = arr[i];
-  return min;
-}
-
-int countPrime(int arr[], const int size) {
-  int primeCtr = 0;
-  for (int i = 0; i < size; i++)
-    if (isPrime(arr[i]))
-      primeCtr++;
-  return primeCtr;
-}
-
-// count palindrome
-int countPalindrome(int arr[], const int size) {
-  int PalindromeCtr = 0;
-  for (int i = 0; i < size; i++)
-    if (isPalindrome(arr[i], reverse(arr[i])))
-      PalindromeCtr++;
-  return PalindromeCtr;
-}
-
-// count the maximum of divisors
-int countDivisors(int arr[], const int size) {
-  int maxDividend = arr[0], divCtr = 0, maxDivs = 0;
-  int i = 0;
-  while (i < size) {
-    divCtr = 0;
-    for (int k = 1; k <= sqrt(arr[k]); k++)
-      if (arr[i] % k == 0)
-	divCtr++;
-    if (maxDivs < divCtr) {
-      maxDivs = divCtr;
-      maxDividend = arr[i];
-    } else if (maxDivs == divCtr) {
-      if (maxDividend != arr[i] && maxDividend < arr[i])
-	maxDividend = arr[i];
+bool isPalindrome(int num) {
+    int original = num;
+    int reversed = 0;
+    
+    while (num > 0) {
+        reversed = reversed * 10 + num % 10;
+        num /= 10;
     }
-    i++;
-  }
-  return maxDividend;
+    
+    return original == reversed;
+}
+
+int countDivisors(int num) {
+    int count = 0;
+    for (int i = 1; i <= num; ++i)
+        if (num % i == 0)
+            count++;
+
+    return count;
 }
 
 int main() {
-  // get the array size
-  int n = 0;
-  cin >> n;
+    int size;
+    cin >> size;
+    
+    int A[size];
+    for (int i = 0; i < size; ++i)
+        cin >> A[i];
+    
+    int maxNum = A[0];
+    int minNum = A[0];
+    
+    int primeCount = 0;
+    int palindromeCount = 0;
+    int maxDivisors = 0;
+    int numWithMaxDivisors = A[0];
 
-  const int size = n;
-  int arr[size];
-  // get the elements of the array
-  for (int i = 0; i < size; i++)
-    cin >> arr[i];
-
-  // find the maximum number
-  int maxNum = findMax(arr, size);
-  cout << "The maximum number : " << maxNum << endl;
-
-  // find the minimum number
-  int minNum = findMin(arr, size);
-  cout << "The minimum number : " << minNum << endl;
-
-  // find number of prime numbers:
-  int primNum = countPrime(arr, size);
-  cout << "The number of prime numbers : " << primNum << endl;
-
-  // find number of palindrome numbers:
-  int palindromeNum = countPalindrome(arr, size);
-  cout << "The number of palindrome numbers : " << palindromeNum << endl;
-  
-  // max number of divisors
-  int maxDivNums = countDivisors(arr, size);
-  cout << "The number that has the maximum number of divisors : " << maxDivNums << endl;
-  
-  return 0;
+    // find maximum number and minimum number
+    for (int i = 0; i < size; ++i) {
+        maxNum = max(maxNum, A[i]);
+        minNum = min(minNum, A[i]);
+        
+        if (isPrime(A[i]))
+	  primeCount++;
+        if (isPalindrome(A[i]))
+	  palindromeCount++;
+        
+        int divisors = countDivisors(A[i]);
+	// check if number with max divisors is larger the pervious one
+        if (divisors > maxDivisors || (divisors == maxDivisors && A[i] > numWithMaxDivisors)) {
+            maxDivisors = divisors;
+            numWithMaxDivisors = A[i];
+        }
+    }
+    
+    cout << "The maximum number : " << maxNum << endl;
+    cout << "The minimum number : " << minNum << endl;
+    cout << "The number of prime numbers : " << primeCount << endl;
+    cout << "The number of palindrome numbers : " << palindromeCount << endl;
+    cout << "The number that has the maximum number of divisors : " << numWithMaxDivisors << endl;
+    
+    return 0;
 }
